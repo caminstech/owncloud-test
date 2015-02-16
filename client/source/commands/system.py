546c66e1ddd06095cf99c5ea53ca6cd2a4dbdf72
Oriol Teixidó <oriol.teixido@gmail.com>
2015-02-16 21:21:37 +0100

    Add Response class

--- client/source/commands/system.py
+++ client/source/commands/system.py
@@ -1,7 +1,13 @@
 import shutil
 
 from ..command import Command
+from ..server import Response
 
 class Copy(Command): 	
   def run(self):
     shutil.copyfile(self.parameters['src'], self.parameters['dst'])
+    response = Response()
+    response.uid = Command.uid
+    response.status = Response.Status.ok
+    response.message = ''
+    return response
--- client/source/server.py
+++ client/source/server.py
@@ -1,47 +1,57 @@
 import json
 import urllib3
 import requests
-
 from enum import Enum
+from urllib.parse import urljoin
 
+from client import Client
 from command import CommandFactory 
 from exception import HttpException
 
-class Server:
+class Response:
   class Status(Enum):
     ok = 'OK'
     error = 'ERROR'
     timeout = 'TIMEOUT'
 
-  _url = None
+  uid = None
+  status = None
+  message = None
 
+  def json(self):
+    return json.dumps({ 'uid': self.uid, 'status': self.status, 'message': self.message })
+
+class Server:
+
+  baseUrl = None
+
+  _client = None
   _factory = None
   _requests = None
 
-  def _parse(self, json):
-    values = {}
-    values['uid'] = json['uid']
-    values['command'] = json['command']
-    values['timeout'] = json['timeout'] if 'timeout' in json else None
-    values['parameters'] = json['parameters'] if 'parameters' in json else {}
-    return values
+  def _createCommand(self, json):
+    command = self._factory.create(json['command'])
+    command.uid = json['uid']
+    command.timeout = json['timeout'] if 'timeout' in json else None
+    command.parameters = json['parameters'] if 'parameters' in json else {}
+    return command
 
-  def __init__(self, url = ''):
-    self._url = url
+  def __init__(self, baseUrl = ''):
+    self.baseUrl = baseUrl
+    self._client = Client()
     self._factory = CommandFactory()
     self._requests = requests
 
   def get(self):
-    response = self._requests.get(self._url)
+    url = urljoin(self.baseUrl, '/' + self._client.uid)
+    response = self._requests.get(url)
     if response.status_code != requests.codes.ok: 
       raise HttpException(response.status_code)
 
-    values = self._parse(response.json())
-    command = self._factory.create(values['command'])
-    command.uid = values['uid']
-    command.timeout = values['timeout']
-    command.parameters = values['parameters']
-    return command
-  
+    return self._createCommand(response.json())
+    
   def send(self, response):
-    self._requests.post(self._url + '/' + response['uid'] , json.dumps(response))
+    url = self.baseUrl
+    url = urljoin(url, '/' + self._client.uid)
+    url = urljoin(url, '/' + response.uid)
+    self._requests.post(url, response.json())
