1df4898db6006213ad5cb2c640472ac5c0feae1d
Oriol Teixidó <oriol.teixido@gmail.com>
2015-02-20 13:07:04 +0100

    Coding server skeleton

--- server/app.py
+++ server/app.py
@@ -1,24 +1,61 @@
 #!/usr/bin/env python3
 
+import json
+import uuid
+import time
+
 from flask import Flask
 
-commands = {
-  'client1': [
-    { 'command': 'copy', 'src': 'a.txt', 'dst': 'b.txt' },
-  ],  
-  'client2': [
-    { 'command': 'wait-file', 'path': 'b.txt' },
-  ], 
+def commandWaitFile(path):
+  return {
+    'uid': str(uuid.uuid4()),
+    'command': 'wait-file',
+    'parameters': {
+      'path': path
+    }
+  }
+
+def commandCopyFile(src, dst):
+  return {
+    'uid': str(uuid.uuid4()),
+    'command': 'copy-file',
+    'parameters': {
+      'src': src,
+      'dst': dst
+    }
+  }
+
+executed = {
 }
+
+pending = {
+  'client-upload': [],
+  'client-download1': [],
+  'client-download2': [],
+}
+
+pending['client-upload'].append(commandCopyFile('file-0.dat', 'file.dat'));
+pending['client-download1'].append(commandWaitFile('file.dat'));
+pending['client-download2'].append(commandWaitFile('file.dat'));
+
+app = Flask(__name__)
 
 @app.route('/client/<client>/command', methods=['GET'])
 def get_command(client):
-  return 'Client %s' % client
+  if len(pending[client]) == 0:
+    return 'Empty pending.', 404
+
+  command = pending[client].pop(0)
+  executed[command.get('uid')] = {
+    'ts-get': time.time() 
+  }
+  return json.dumps(command)
 
 @app.route('/client/<client>/command/<command>', methods=['POST'])
 def post_command(client, command):
-  return 'Client %s - %s' % client, command
+  executed[command]['time-post'] = time.time()
+  return json.dumps(executed[command])
 
 if __name__ == '__main__':
-  app = Flask(__name__)
+  app.run(debug=True)
   app.run()
