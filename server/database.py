494c385fd4f60395d0bbb5bb3cfad9d868302ee6
Oriol Teixidó <oriol.teixido@gmail.com>
2015-03-03 18:59:49 +0100

    Database class implemented with tests

--- server/database.py
+++ server/database.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+class NotFoundException(Exception):
+  pass
+
 class DatabaseSQLite:
   _db = None
 
@@ -9,8 +12,29 @@ class DatabaseSQLite:
   def  __del__(self):
     self._db.close()
       
-  def find(self, sql, variables = {}):
+  def find(self, table, key, values):
+    sql = 'select * from %s where %s=:%s' % (table, key, key)
     cursor = self._db.cursor()
-    cursor.execute(sql, variables)
-    return cursor.fetchone()
+    cursor.execute(sql, values)
+    result = cursor.fetchone()
+    cursor.close()
+    return result
 
+  def insert(self, table, values):  
+    names = [k for k in values.keys()]
+    variables = [':' + k for k in values.keys()]
+    sql = 'insert into %s (%s) values (%s)' % (table, ','.join(names), ','.join(variables))
+    cursor = self._db.cursor()
+    cursor.execute(sql, values)
+    cursor.close
+
+  def update(self, table, key, values):
+    names = [k for k in values.keys()]
+    names.remove(key)
+    sets = [k + '=:' + k for k in names]
+    sql = 'update %s set %s where %s=:%s' % (table, ','.join(sets), key, key)
+    cursor = self._db.cursor()
+    cursor.execute(sql, values)
+    if cursor.rowcount != 1:
+      raise NotFoundException()
+      
--- server/test/database_tests.py
+++ server/test/database_tests.py
@@ -2,30 +2,60 @@ from mockito import *
 import unittest
 
 from sqlite3 import OperationalError
-from database import DatabaseSQLite
+from database import DatabaseSQLite, NotFoundException
 
 class DatabaseSQLiteTest(unittest.TestCase):
-
   def createTable(self):
     cursor = self.database._db.cursor()
-    cursor.execute('create table test (uid text)')
+    cursor.execute('create table test (uid text, name text, description text)')
   
   def insert(self, uid):
     cursor = self.database._db.cursor()
-    cursor.execute('insert into test values (:uid)', { 'uid': uid })
+    cursor.execute('insert into test values (:uid, :name, :description)', {'uid': uid, 'name': 'test-name', 'description': 'test-description'})
+
+  def find(self, uid):
+    cursor = self.database._db.cursor()
+    cursor.execute('select * from test where uid=:uid', {'uid': uid})
+    return cursor.fetchone()
       
   def setUp(self):
     self.database = DatabaseSQLite(':memory:')
 
   def testFindTableNotExists(self):
-    self.assertRaises(OperationalError, self.database.find, ('select * from test'))
+    self.assertRaises(OperationalError, self.database.find, 'test', 'uid', {'uid': 'test-uid'})
 
   def testFindNotFound(self):
     self.createTable()
-    self.assertIsNone(self.database.find('select * from test'))
+    self.assertIsNone(self.database.find('test', 'uid', {'uid': 'test-uid'}))
 
   def testFind(self):
     self.createTable()
     self.insert('test-uid')
-    (uid,) = self.database.find('select * from test where uid = :uid', { 'uid': 'test-uid' })
+    (uid, name, description) = self.database.find('test', 'uid', {'uid': 'test-uid'})
     self.assertEquals('test-uid', uid)
+    self.assertEquals('test-name', name)
+    self.assertEquals('test-description', description)
+
+  def testInsertTableNotExists(self):
+    self.assertRaises(OperationalError, self.database.insert, 'test', {'uid': 'test-uid'})
+
+  def testInsert(self):
+    self.createTable()
+    self.database.insert('test', {'uid': 'test-uid', 'name': 'test-name', 'description': 'description-test'})
+    self.assertIsNotNone(self.find('test-uid'))
+
+  def testUpdateTableNotExists(self):
+    self.assertRaises(OperationalError, self.database.update, 'test', 'uid', {'uid': 'test-uid', 'name': 'test-name', 'description': 'description-test'})
+
+  def testUpdateNotFound(self):
+    self.createTable()
+    self.assertRaises(NotFoundException, self.database.update, 'test', 'uid', {'uid': 'test-uid', 'name': 'test-name-updated', 'description': 'test-description-updated'})
+
+  def testUpdate(self):
+    self.createTable()
+    self.insert('test-uid')
+    self.database.update('test', 'uid', {'uid': 'test-uid', 'name': 'test-name-updated', 'description': 'test-description-updated'})
+    (uid, name, description) = self.find('test-uid')
+    self.assertEquals('test-uid', uid)
+    self.assertEquals('test-name-updated', name)
+    self.assertEquals('test-description-updated', description)
