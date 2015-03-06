ce5e42f796abec6e41fbbebca28812c77f6658e8
Oriol Teixidó <oriol.teixido@gmail.com>
2015-03-06 12:48:34 +0100

    Remove unused exceptions

--- server/exception.py
+++ server/exception.py
@@ -1,11 +1,2 @@
 class NotFoundException(Exception):
   pass
-
-class CommandNotFoundException(Exception):
-  pass
-
-class EmptyCommandsException(Exception):
-  pass
-
-class WaitingClientsException(Exception):
-  pass
